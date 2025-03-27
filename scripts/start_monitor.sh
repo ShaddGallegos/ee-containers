3209af16e2e3bbda79f9968cd1477a6f33132eac
Shadd Gallegos <shadd.gallegos@gmail.com>
2025-03-27 10:16:55 -0600

    still fixing

--- scripts/start_monitor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tmux new-session -d -s podman-monitor
-tmux split-window -v -p 30
-tmux select-pane -t 0
-tmux send-keys "watch podman ps" C-m
-tmux select-pane -t 1
-tmux send-keys "watch podman images" C-m
